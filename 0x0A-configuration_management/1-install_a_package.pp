e3074799d31c25fb4953bd68fee3b3fad9c026d4
Ian Kerich <iankerich254@gmail.com>
2024-06-28 21:23:32 +0300

    Install a package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/bin', '/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
+}
